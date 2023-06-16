2356fac4fcf93a0adbb9d03f0fe867ace9c33fb1
Adeniran John <adeniranjohn2016@gmail.com>
2023-06-16 12:50:01 +0100

    added publish manager

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 class Post(models.Model):
     
     class Status(models.TextChoices):
@@ -17,6 +22,9 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    
+    objects = models.Manager()
+    published = PublishedManager()
     
     def __str__(self) -> str:
         return self.title
