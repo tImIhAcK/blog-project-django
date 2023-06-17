493322504d5001c009959e298a9156a904eb726c
Adeniran John <adeniranjohn2016@gmail.com>
2023-06-17 06:54:20 +0100

    conical url

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -28,6 +28,10 @@ class Post(models.Model):
     
     def __str__(self) -> str:
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.id])
+    
     
     class Meta:
         ordering = ['-publish']
